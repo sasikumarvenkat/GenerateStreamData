973718cfa75a11e03c3d72de9d2d3ad4e12e2135
Sasikumar Venkatesh <sasikumar.venkatesh@walmart.com>
2021-07-08 13:55:40 +0530

    Added Simulation Data

--- src/com/demo/truck/data/GenreateRandomTruckInfoJson.java
+++ src/com/demo/truck/data/GenreateRandomTruckInfoJson.java
@@ -7,15 +7,23 @@ import static com.demo.truck.data.GenrateTruckInfo.generateSensorData;
 import static com.demo.truck.data.GenrateTruckInfo.generateSpeed;
 import static com.demo.truck.data.GenrateTruckInfo.randomLonLat;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Random;
 
 public class GenreateRandomTruckInfoJson {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 
 		while (true) {
-			for (int i = 1; i <= new Random().nextInt(15); i++) {
+			File file = new File("/Users/s0v00eo/demo-workspace/GenerateStreamData/simulated-data/truck_info.json");
+			FileWriter fw = new FileWriter(file, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			int records = new Random().nextInt(15);
+			for (int i = 1; i <= records; i++) {
 				VehicleInfo vehicleInfo = new VehicleInfo();
 				vehicleInfo.setVehicleId(generateRandomVehicleId());
 				vehicleInfo.setVehicleType(generateRandomVehicleType());
@@ -25,10 +33,13 @@ public class GenreateRandomTruckInfoJson {
 				vehicleInfo.setSpeed(generateSpeed(0, 200));
 				Map<String, String> jw = randomLonLat(12, 15, 70, 80);
 				vehicleInfo.setLocation(new Location(Double.parseDouble(jw.get("J")), Double.parseDouble(jw.get("W"))));
-				System.out.println(vehicleInfo);
+				// Write the vehicleInfo data into a file
+				bw.append(vehicleInfo.toString() + "\n");
 			}
-			System.out.println("-----");
-			Thread.sleep(1000);
+			System.out.println("written " + records + " to the file. ");
+			bw.flush();
+			bw.close();
+			Thread.sleep(300);
 		}
 	}
 }
